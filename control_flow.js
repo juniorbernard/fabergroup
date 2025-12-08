78d59d06123ce1d615efc44f80ebdc544c3e39ab
Bernard-Junior <juniorbernardbeya@gmail.com>
2025-12-08 16:00:48 +0100

    Ajout de nouveaux fichiers js et html pour control_flow

---
+++ control_flow.js
@@ -0,0 +1,50 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+  accessLevel = "full access granted";
+} else if (userRole === "manager") {
+  accessLevel = "limited access granted";
+} else {
+  accessLevel = "no access granted";
+}
+
+// console.log("accessLevel:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole === "admin"){
+        userMessage = "Welcome back, Admin!";
+    } else {
+        userMessage = "Welcome back, User!";
+    }
+}else{
+    userMessage = "Please log in to continue.";
+}
+
+// console.log("accessLevel:", accessLevel);
+// console.log("userMessage:", userMessage);
+
+let userType = "admin";
+let userCategory;
+
+switch(userType){
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+        break;
+}
+
+console.log("accessLevel:", accessLevel);
+console.log("userMessage:", userMessage);
+console.log("userCategory:", userCategory);
