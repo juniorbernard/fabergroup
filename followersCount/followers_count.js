efa57e503999d8c568eee2ce00cb618015eeb2b0
Bernard-Junior <juniorbernardbeya@gmail.com>
2025-12-09 08:21:52 +0100

    Ajout de la logique pour décrémenter et réinitialiser le compteur de followers dans followers_count.js

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,6 +6,22 @@ function increaseCount() {
   checkCountValue(); // Vérifier si le nombre de followers atteint une valeur spécifique
 }
 
+function decreaseCount() {
+    count--; // Décrémenter le nombre de followers de 1
+    if (count < 0) {
+        count = 0; // Empêcher le nombre de followers de devenir négatif
+    }
+    displayCount(); // afficher le nouveau nombre de followers
+    checkCountValue(); // Vérifier si le nombre de followers atteint une valeur spécifique
+}
+
+function resetCount() {
+    count = 0; // Réinitialiser le nombre de followers à 0
+    displayCount(); // afficher le nouveau nombre de followers
+    checkCountValue(); // Vérifier si le nombre de followers atteint une valeur spécifique
+    
+}
+
 function displayCount() {
   document.getElementById("countDisplay").innerHTML = count; // Mettre à jour l'affichage du nombre de followers
 }
@@ -15,5 +31,7 @@ function checkCountValue() {
     alert("Félicitations ! Vous avez atteint 10 followers !");
   } else if (count === 50) {
     alert("Incroyable ! Vous avez atteint 50 followers !");
+  }else if (count === 100) {
+    alert("Wow ! Vous avez atteint 100 followers !");
   }
 }
