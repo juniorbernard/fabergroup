04c89ea15b77cce0b1bbedf8c8c0883db6aa8cff
Bernard-Junior <juniorbernardbeya@gmail.com>
2025-12-10 23:29:24 +0100

    Ajout du fichier restaurant_menu.html et implémentation de la logique pour afficher le menu du restaurant dans restaurant_menu.js

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,24 @@
+const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
+const mainCourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
+const dessertMenu = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
+
+const breakfastMenuItemsHTML = breakfastMenu
+  .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+  .join("");
+
+document.getElementById("breakfastMenuItems").innerHTML =
+  breakfastMenuItemsHTML;
+
+let mainCourseItem = " ";
+mainCourseMenu.forEach((item, index)=>{
+    mainCourseItem += `Article ${index + 1} : ${item} <br>`;
+});
+
+document.getElementById("maincourseMenuItems").innerHTML = mainCourseItem;
+
+let dessertItem = " ";
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `Dessert ${i + 1} : ${dessertMenu[i]} <br>`;
+}
+
+document.getElementById("dessertMenuItems").innerHTML = dessertItem;
