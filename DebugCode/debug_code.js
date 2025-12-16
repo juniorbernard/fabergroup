a23410c0b99471aa47d392d4f9c9b4e97e6cf5c0
Bernard-Junior <juniorbernardbeya@gmail.com>
2025-12-16 16:14:09 +0100

    Refactor performOperation to include multiple arithmetic operations and enhance result display

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,26 +1,63 @@
-function performOperation(){
-    // avoir les valeurs des inputs
-    const input1 = parseFloat(document.getElementById("input1").value);
-    const input2 = parseFloat(document.getElementById("input2").value);
+function performOperation() {
+  // avoir les valeurs des inputs
+  const input1 = parseFloat(document.getElementById("input1").value);
+  const input2 = parseFloat(document.getElementById("input2").value);
 
-    if(!isNaN(input1) && !isNaN(input2)){
-       let result = multiply(input1, input2);
+  if (!isNaN(input1) && !isNaN(input2)) {
+    const multiply_result = multiply(input1, input2);
+    const sumation_result = addition(input1, input2);
+    const subtraction_result = subtraction(input1, input2);
+    const division_result =
+      input2 === 0 ? "Cannot divide by zero" : division(input1, input2);
 
-       displayResult(result);
-    }else{
-        displayResult('Please enter valid numbers.');
-    }
+    displayResult({
+      multiply_result,
+      sumation_result,
+      subtraction_result,
+      division_result,
+    });
+  } else {
+    displayResult("Please enter valid numbers.");
+  }
 }
 
-function multiply(num1, num2){
-    // Introduce a debugger to pause execution
-    debugger;
-    // multiply the numbers
-    return num1 * num2;
+function multiply(num1, num2) {
+  // Introduce a debugger to pause execution
+  debugger;
+  // multiply the numbers
+  return num1 * num2;
 }
 
-function displayResult(result){
-    // display the result
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = `The result is: ${result}`;
+function addition(num1, num2) {
+  // add the numbers
+  return num1 + num2;
+}
+
+function subtraction(num1, num2) {
+  // subtract the numbers
+  return num1 - num2;
+}
+
+function division(num1, num2) {
+  // divide the numbers
+  return num1 / num2;
+}
+
+function displayResult(result) {
+  const resultElement = document.getElementById("result");
+  if (typeof result === "string") {
+    resultElement.innerText = result;
+    return;
+  }
+  const {
+    multiply_result,
+    sumation_result,
+    subtraction_result,
+    division_result,
+  } = result;
+
+  resultElement.innerText = `Multiply result is : ${multiply_result}
+Summation result is : ${sumation_result}
+Subtraction result is : ${subtraction_result}
+Division result is : ${division_result}`;
 }
