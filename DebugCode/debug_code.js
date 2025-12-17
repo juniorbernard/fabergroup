0d0f038f6edddbc93dc4c8f9546b8aae15d4059f
Bernard-Junior <juniorbernardbeya@gmail.com>
2025-12-17 12:01:55 +0100

    Refactor performOperation to include modulo operation and update result messages to French

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -8,16 +8,18 @@ function performOperation() {
     const sumation_result = addition(input1, input2);
     const subtraction_result = subtraction(input1, input2);
     const division_result =
-      input2 === 0 ? "Cannot divide by zero" : division(input1, input2);
+      input2 === 0 ? "La division par zero est impossible." : division(input1, input2);
+    const mudulo_result = mudulo(input1, input2);
 
     displayResult({
       multiply_result,
       sumation_result,
       subtraction_result,
       division_result,
+      mudulo_result,
     });
   } else {
-    displayResult("Please enter valid numbers.");
+    displayResult("Veillez inserer un nombre valide.");
   }
 }
 
@@ -43,6 +45,11 @@ function division(num1, num2) {
   return num1 / num2;
 }
 
+function mudulo(num1, num2) {
+  // divide the numbers
+  return num1 % num2;
+}
+
 function displayResult(result) {
   const resultElement = document.getElementById("result");
   if (typeof result === "string") {
@@ -54,10 +61,12 @@ function displayResult(result) {
     sumation_result,
     subtraction_result,
     division_result,
+    mudulo_result,
   } = result;
 
-  resultElement.innerText = `Multiply result is : ${multiply_result}
-Summation result is : ${sumation_result}
-Subtraction result is : ${subtraction_result}
-Division result is : ${division_result}`;
+  resultElement.innerText = `Le produit est de : ${multiply_result}
+La somme est de : ${sumation_result}
+Le reste est de : ${subtraction_result}
+Le quotient est de : ${division_result} 
+Le reste de la division est de : ${mudulo_result}`;
 }
